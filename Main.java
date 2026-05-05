f5504c25c6563086bac1266f215aaee65363bc58
Lucas <lucas111machado.lm@gmail.com>
2026-05-05 00:53:36 -0300

    criando classes

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main (String[] args) {
+        Person male = new Person();
+        male.name ="João";
+        male.age = 12;
+        Person female = new Person();
+        female.name = "Maria";
+        female.age = 10;
+        System.out.println("Male name: " + male.name + " age: " + male.age);
+        System.out.println("Female name: " + female.name + " age: " + female.age);
+    }
+}
