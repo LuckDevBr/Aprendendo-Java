767dbc5954e29105c666c1517172141ec3a6420c
Lucas <lucas111machado.lm@gmail.com>
2026-05-07 11:37:28 -0300

    praticando

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         Person male = new Person();
         male.name ="João";
         male.age = 12;
